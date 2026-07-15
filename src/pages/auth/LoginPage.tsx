b6984268e827029dac88d6f865435c6d9d128b90
cilkotron <sanjabudic2907@gmail.com>
2026-07-15 14:00:51 +0200

    Connect LoginPage

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -12,7 +12,7 @@ export const LoginPage = () => {
     const { setAuth } = useAuth();
     const navigate = useNavigate();
 
-    const handleSubmit = async (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.SubmitEvent) => {
         e.preventDefault();
         setError('');
         setLoading(true);
