f0bfe52315aeebde697fcf3bb6d510361be24c59
cilkotron <sanjabudic2907@gmail.com>
2026-07-15 11:58:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+        <section>
+            <div className="font-bold flex justify-center">Admin Dashboard</div>
+        </section>
+    </>
+  )
+}
+
+export default App
